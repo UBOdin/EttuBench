57bfbddb2210ec47d426209ef968ef4671e1e80c
Duc Thanh Anh Luong <luongthanhanhduc@gmail.com>
2017-09-06 18:56:42 -0400

    change some text and size in Figure 2

--- script_figure_2.R
+++ script_figure_2.R
@@ -7,23 +7,24 @@ library(ggplot2)
 comparison <- read.csv(file = "./data/result.csv", header = TRUE)
 comparison$dataset <- factor(comparison$dataset, 
                              levels = c("IIT Bombay Dataset", 
-                                        "Local Exam Dataset", 
+                                        "UB Exam Dataset", 
                                         "PocketData-Google+"))
 
 ggplot(data = comparison, aes(x = metric, y = silhouette, fill = regularization)) + 
   geom_bar(position="dodge", stat="identity") + facet_grid(~ dataset) + 
   ylab("Average Silhouette Coefficient") + xlab("Metric") + 
   theme_bw(base_size = 18) + theme(legend.position = "top") + scale_fill_grey() +
-  ggsave(filename = "./figure/compare_silhouette.pdf")
+  ggsave(filename = "./figure/compare_silhouette.pdf", height = 5)
 
 ggplot(data = comparison, aes(x = metric, y = beta_cv, fill = regularization)) + 
   geom_bar(position="dodge", stat="identity") + facet_grid(~ dataset) + 
-  ylab("Average Silhouette Coefficient") + xlab("Metric") + 
+  ylab("BetaCV") + xlab("Metric") + 
   theme_bw(base_size = 18) + theme(legend.position = "top") + scale_fill_grey() +
-  ggsave(filename = "./figure/compare_betacv.pdf")
+  ggsave(filename = "./figure/compare_betacv.pdf", height = 5)
 
 ggplot(data = comparison, aes(x = metric, y = dunn, fill = regularization)) + 
   geom_bar(position="dodge", stat="identity") + facet_grid(~ dataset) + 
-  ylab("Average Silhouette Coefficient") + xlab("Metric") + 
+  ylab("Dunn Index") + xlab("Metric") + 
   theme_bw(base_size = 18) + theme(legend.position = "top") + scale_fill_grey() +
-  ggsave(filename = "./figure/compare_dunn.pdf")
+  ggsave(filename = "./figure/compare_dunn.pdf", height = 5)
+
